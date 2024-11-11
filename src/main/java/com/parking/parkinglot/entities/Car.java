282481dc8b51612b3b38af849cee78317ca1c098
Luca Rusu <lucarusu0303@gmail.com>
2024-11-11 08:58:50 +0200

    Part 5 : created the entities

---
+++ src/main/java/com/parking/parkinglot/entities/Car.java
@@ -0,0 +1,53 @@
+package com.parking.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+    private Long id;
+
+    private User owner;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    private String licensePlate;
+
+    @Column(name = "license_plate")
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    private String parkingSpot;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    @Column(name = "parking_spot")
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(String parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+}
