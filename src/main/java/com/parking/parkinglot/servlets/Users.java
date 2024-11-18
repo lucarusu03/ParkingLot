7214ab971b88ba43ef164f287a9871d3bdedd3ab
Luca Rusu <lucarusu0303@gmail.com>
2024-11-18 09:19:41 +0200

    Part 7 : Created the EJB

---
+++ src/main/java/com/parking/parkinglot/servlets/Users.java
@@ -0,0 +1,32 @@
+package com.parking.parkinglot.servlets;
+
+import com.parking.parkinglot.common.UserDto;
+import com.parking.parkinglot.ejb.UserBean;
+import jakarta.inject.Inject;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "Users", value = "/Users")
+public class Users extends HttpServlet {
+
+    @Inject
+    UserBean userBean;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse
+            response) throws ServletException, IOException {
+        List<UserDto> users = userBean.findAllUsers();
+        request.setAttribute("users", users);
+        request.setAttribute("activePage", "Users");
+        request.getRequestDispatcher("/WEB-INF/pages/users.jsp").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse
+            response) throws ServletException, IOException {
+    }
+}
--- src/main/webapp/WEB-INF/pages/menu.jsp
+++ src/main/webapp/WEB-INF/pages/menu.jsp
@@ -18,7 +18,9 @@
               'Cars' ? 'active' : ''}" aria-current="page" href="${pageContext.request.contextPath}/Cars">Cars</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link
+              ${activePage eq
+              'Users' ? 'active' : ''}" aria-current="page" href="${pageContext.request.contextPath}/Users">Users</a>
           </li>
           <li class="nav-item">
             <a class="nav-link disabled">Disabled</a>
